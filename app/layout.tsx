688a4a0dbdbb8d291d1d1707e763084599734b2c
Rahul Krishna <rahulkrishna.web@gmail.com>
2026-07-11 13:24:28 +0530

    feat: integrate Google Tag Manager into root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,10 +17,33 @@ export default function RootLayout({
       className="h-full antialiased"
     >
       <head>
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-NRCX8PH');`,
+          }}
+        />
+        {/* End Google Tag Manager */}
         <link rel="icon" href="/favicon.ico" />
         <link rel="stylesheet" href="https://fonts.cdnfonts.com/css/proxima-nova-2" />
       </head>
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-NRCX8PH"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
+        {children}
+      </body>
     </html>
   );
 }
